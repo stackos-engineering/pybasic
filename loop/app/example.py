11338618d40ef18ef402b97995f79095d707cbef
stackoseng <contact@stackos.io>
2022-07-30 16:50:21 +0200

    add unbuffered output to view logs in docker daemon mode

---
+++ loop/app/example.py
@@ -0,0 +1,17 @@
+from time import sleep
+import requests
+import datetime
+
+print("Starting example python program")
+
+while True:
+    now = datetime.datetime.now()
+    print(now)
+    print("Loop Part A")
+    sleep(2)
+    print("Loop Part B")
+    sleep(2)
+    print("Loop Part C")
+    sleep(2)
+    print("Loop Part D")
+    sleep(2)
